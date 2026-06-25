96e86dbf425a0f71922ef39059b79ded8c34bd9d
sisonjcs <jcssison1@gmail.com>
2026-06-25 10:45:34 +0800

    feat: added save and load tasks

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,13 @@
 
 <script setup lang="ts">
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { onMounted } from 'vue';
+import { useTaskStore } from './stores/taskStore';
+
+const taskStore = useTaskStore()
+
+onMounted(async() => {
+  await taskStore.loadTasks()
+  console.log("Loaded tasks")
+})
 </script>
--- src/stores/taskStore.ts
+++ src/stores/taskStore.ts
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
+import { Preferences } from '@capacitor/preferences'
 
 interface Task {
     id: number
@@ -9,12 +10,37 @@ interface Task {
 }
 
 export const useTaskStore = defineStore('tasks', () => {
+    const STORAGE_KEY = 'taskflow_tasks'
+    const ID_KEY = 'taskflow_id'
+
     const tasks = ref<Task[]>([])
     const nextId = ref(1)
 
     const totalCount = computed(() => tasks.value?.length)
     const doneCount = computed(() => tasks.value?.filter(t => t.done).length)
     const pendingCount = computed(() => tasks.value?.filter(t => !t.done).length)
+
+    const saveTasks = async() => {
+        await Preferences.set({
+            key: STORAGE_KEY,
+            value: JSON.stringify(tasks.value)
+        })
+        await Preferences.set({
+            key: ID_KEY,
+            value: JSON.stringify(nextId.value)
+        })
+    }
+
+    const loadTasks = async() => {
+        const { storageValue }: any = await Preferences.get({key: STORAGE_KEY})
+        const { taskId }: any = await Preferences.get({key: ID_KEY})
+        if (storageValue.value) {
+           tasks.value = storageValue.value
+        }
+        if (taskId.value) {
+            nextId.value = taskId.value
+        }
+    }
 
     function addTask(name: string) {
         if (name) {
@@ -25,6 +51,7 @@ export const useTaskStore = defineStore('tasks', () => {
             }
 
             tasks.value.push(newTask)
+            saveTasks()
         }
     }
 
@@ -32,6 +59,7 @@ export const useTaskStore = defineStore('tasks', () => {
         const foundTask = tasks.value.find(t => t.id === id)
         if (foundTask) {
             foundTask.done = !foundTask.done
+            saveTasks()
         }
     }
 
@@ -39,6 +67,7 @@ export const useTaskStore = defineStore('tasks', () => {
         const foundTask = tasks.value.find(t => t.id === id)
         if (foundTask) {
             tasks.value = tasks.value.filter(t => t.id !== id)
+            saveTasks()
         }
     }
 
@@ -47,10 +76,11 @@ export const useTaskStore = defineStore('tasks', () => {
 
         if (foundTask && photoPath) {
             foundTask.photo = photoPath
+            saveTasks()
         }
     }
     
-    return ({ tasks, doneCount, pendingCount, totalCount, addTask, toggleTask, removeTask, addPhoto })
+    return ({ tasks, doneCount, pendingCount, totalCount, addTask, toggleTask, removeTask, addPhoto, loadTasks })
 }, {
     persist: true
 })
